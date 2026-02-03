2464e364b9d2e673088a94509a9cfc12f7a58854
GAGAN GOWDA S <gogagan0323@gmail.com>
2026-02-03 08:27:55 +0530

    Add files via upload

---
+++ Pr4.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+struct book {
+    int id;
+    char title[50];
+    char author[50];
+};
+int main() {
+    struct book b1= {101, "C Programming", "Dennis Ritchie"};
+    struct book b2= {102, "Data Structures", "Mark Weiss"};
+    struct book b3= {103, "Operating Systems", "Silberschatz"};
+    struct book *books[3]= {&b1, &b2, &b3};
+    int searchID, i, found=0;
+    printf("Enter book ID to search:");
+    scanf("%d", &searchID);
+    for(i=0; i<3; i++) {
+        if(books[i]->id == searchID) {
+            printf("\nbook found:\n");
+            printf("ID: %d\n", books[i]->id);
+            printf("Title: %s\n", books[i]->title);
+            printf("Author: %s\n", books[i]->author);
+            found = 1;
+            break;
+        } 
+    }
+
+
+}
